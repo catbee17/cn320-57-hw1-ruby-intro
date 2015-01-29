c3257d654195208528a63230913f6c82a136d715
chompoonick5510613325 <5510613325@student.tu.ac.th>
2015-01-29 18:45:01 +0000

    finish

--- lib/part2.rb
+++ lib/part2.rb
@@ -1,11 +1,25 @@
 def hello(name)
-  # YOUR CODE HERE
+  a=name.to_str
+  return "Hello, "+a
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  b=s.downcase
+  a=b[0]
+  if a=="a"||a=="e"||a=="i"||a="o"||a="u"
+    return false
+  else return ture 
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  
+  if(s==0 || s==1)
+    if(s[s.length-1]==0 && s[s.length-2]==0)
+      return true
+    else
+      return false
+    end
+    return false
+  end
 end
