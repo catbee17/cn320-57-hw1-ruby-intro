3024c6bc3b4db5d60d5de2824004a91af189d93d
chompoonick5510613325 <5510613325@student.tu.ac.th>
2015-01-30 17:38:20 +0000

    finish

--- lib/part2.rb
+++ lib/part2.rb
@@ -6,20 +6,29 @@ end
 def starts_with_consonant? s
   b=s.downcase
   a=b[0]
-  if a=="a"||a=="e"||a=="i"||a="o"||a="u"
+  if (a=="a"|| a=="e"|| a=="i"|| a="o"|| a="u")
     return false
   else return ture 
   end
 end
 
+
 def binary_multiple_of_4? s
+  i = 0
+  while(i < s.length)
+     if(s[i]==0 || s[i]==1)
+       i = i+1
+     else
+       return false
+     end
+  return false 
+  end
   
-  if(s==0 || s==1)
     if(s[s.length-1]==0 && s[s.length-2]==0)
       return true
     else
       return false
     end
     return false
-  end
+  
 end
--- lib/part3.rb
+++ lib/part3.rb
@@ -1,3 +1,22 @@
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn,price)
+        @isbn=isbn
+        @price=price
+        
+        if(isbn=="" && price == 0)
+            return false
+        else
+            price = price_as_string(price)
+            info
+        end
+    end
+    
+    def info
+        "ISBN:#@isbn,PRICE:#@price"
+    end
+    
+    def price_as_string(price)
+        a=price/1.00
+        return "$"+a.to_str
+    end    
 end
