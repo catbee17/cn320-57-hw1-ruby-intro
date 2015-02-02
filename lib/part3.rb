ea56ea58cde6c3b20c75a59110432422c340ef1d
chompoonick5510613325 <5510613325@student.tu.ac.th>
2015-02-02 14:36:59 +0000

    finish

--- lib/part3.rb
+++ lib/part3.rb
@@ -1,22 +1,30 @@
 class BookInStock
     def initialize(isbn,price)
+        if(isbn=="" && price<1)
+            raise ArgumentError
+        end
         @isbn=isbn
         @price=price
+    end
+    def isbn
+        @isbn
+    end
+    def isbn=(x)
+        @isbn=x
+    end
+    def price
+        @price
+    end
+    def price=(y)
+        @price=y
+    end
+    
         
-        if(isbn=="" && price == 0)
-            return false
-        else
-            price = price_as_string(price)
-            info
-        end
-    end
-    
-    def info
-        "ISBN:#@isbn,PRICE:#@price"
-    end
-    
-    def price_as_string(price)
-        a=price/1.00
-        return "$"+a.to_str
+    def price_as_string
+        a='%.2f' % (@price.to_f)
+        c=a.to_str
+        d="$"
+        d<< c
+        return d
     end    
 end
